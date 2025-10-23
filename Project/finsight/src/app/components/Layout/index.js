7f1b219452411f8808f983c6586961f03891db6c
Vikram Kondagunta <96449200+vik-kon@users.noreply.github.com>
2025-10-23 16:24:12 -0500

    Fixed the layout issues and made the dashboard horizontal

--- Project/finsight/src/app/components/Layout/index.js
+++ Project/finsight/src/app/components/Layout/index.js
@@ -6,9 +6,7 @@ import styles from "./Layout.module.css";
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
-      <div className={styles.column_left}>Left Column</div>
       <div className={styles.column_middle}>{children}</div>
-      <div className={styles.column_right}>Right Column</div>
     </div>
   );
 }
