97bc4e58c09c6edf60191dd6b31e790e234a4277
Vikram Kondagunta <96449200+vik-kon@users.noreply.github.com>
2025-10-28 21:44:42 -0500

    Created Scaffold for Sentiment Signals

--- Project/finsight/src/app/components/Blocks/NewsFeed.js
+++ Project/finsight/src/app/components/Blocks/NewsFeed.js
@@ -1,7 +1,7 @@
 
 export default function NewsFeed() {
     return (
-        <div className = "block news-feed">
+        <div>
             <h2>News Feed</h2>
           <p>Latest stock market and company news updates.</p>
         </div>
