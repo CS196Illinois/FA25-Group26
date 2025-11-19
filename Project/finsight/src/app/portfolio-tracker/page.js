35479494bc7297cb330abb7b6cc47e8458f28527
ashleylibasci <ashleylibasci@icloud.com>
2025-11-18 20:52:45 -0600

    changed some text so that it was more visible

--- Project/finsight/src/app/portfolio-tracker/page.js
+++ Project/finsight/src/app/portfolio-tracker/page.js
@@ -4,7 +4,7 @@ import NewsFeed from "../components/Blocks/NewsFeed";
 export default function PortfolioTracker() {
     return (
         <div style={{ padding: '2rem'}}> 
-            <h1>Portfolio Tracker</h1>
+            <h1 style={{ color: 'white' }}>Portfolio Tracker</h1>
             <PortfolioPerformance />
             <NewsFeed />
         </div>
